0421b5add44eecc339587790e32d93982f13845e
Leon Verhelst <verhelst@unbc.ca>
2013-09-05 21:00:51 -0700

    Inital Commit

---
+++ BergVerhelstCompiler/src/bergverhelstcompiler/BergVerhelstCompiler.java
@@ -0,0 +1,17 @@
+package bergverhelstcompiler;
+
+/**
+ *
+ * @author Emery Berg, Leon Verhelst
+ */
+public class BergVerhelstCompiler {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Print Motivational Message
+        System.out.println("Emery smells awful.");
+    }
+}
+

787d83a5647d948faf60b6b721d4d72ddcb3167d
Leon Verhelst <verhelst@unbc.ca>
2013-09-09 20:11:37 -0700

    File Reader, Basic Input test in Main

--- BergVerhelstCompiler/src/bergverhelstcompiler/BergVerhelstCompiler.java
+++ BergVerhelstCompiler/src/bergverhelstcompiler/BergVerhelstCompiler.java
@@ -1,5 +1,8 @@
 package bergverhelstcompiler;
-
+import FileIO.FileReader;
+import Lexeme.Token;
+import Scanner.Scanner;
+import java.io.IOException;
 /**
  *
  * @author Emery Berg, Leon Verhelst
@@ -12,6 +15,33 @@ public class BergVerhelstCompiler {
     public static void main(String[] args) {
         // Print Motivational Message
         System.out.println("Leon smells awful.");
+        //FileReader reader = new FileReader(args[0]);
+        FileReader reader = new FileReader("src/cs13Test.cs13");
+        String fileContents = null;
+        try{
+            fileContents = reader.readFileToString();
+        //Since FileNotFoundException is a IOException, we can use IOException to 
+        //Catch both types of exceptions
+        }catch(IOException e){
+            System.out.println(e.toString());
+        }
+        if(fileContents != null){
+            Scanner scn = new Scanner(fileContents);
+            Boolean eof = false;
+            Token currentToken;
+            while(!eof){
+                currentToken = scn.getToken();
+                //Print out current token
+                System.out.println(currentToken.getLexeme() + " -> (" + currentToken.getName() + ", " + currentToken.getAttribute_Value() == null ? "-" : currentToken.getAttribute_Value());
+                if(currentToken.getName() == Token.token_Type.ENDFILE){
+                    eof = true;
+                }
+            }
+        }
+             
+        
+                
+        
     }
 }
 

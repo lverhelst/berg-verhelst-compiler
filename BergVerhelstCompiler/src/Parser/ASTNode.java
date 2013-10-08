0f4659e45779f4849d5ea2cf8504bab4f61731e5
Leon Verhelst <verhelst@unbc.ca>
2013-10-08 15:38:58 -0700

    Added ASTNode

---
+++ BergVerhelstCompiler/src/Parser/ASTNode.java
@@ -0,0 +1,76 @@
+package Parser;
+
+/**
+ *
+ */
+public class ASTNode {
+    public class programNode extends ASTNode{
+        
+    }
+    
+    public class funcDeclarationNode extends ASTNode{
+        
+    }
+    
+    public class varDeclarationNode extends ASTNode{
+        
+    }
+    
+    public class parameterNode extends ASTNode{
+        
+    }
+    
+    public class compoundNode extends ASTNode{
+        
+    }
+    
+    public class assignmentNode extends ASTNode{
+        
+    }
+    
+    public class ifNode extends ASTNode{
+        
+    }
+    
+    public class loopNode extends ASTNode{
+        
+    }
+    
+    public class markerNode extends ASTNode{
+        
+    }
+    
+    public class returnNode extends ASTNode{
+        
+    }
+    
+    public class branchNode extends ASTNode{
+        
+    }
+    
+    public class caseNode extends ASTNode{
+        
+    }
+    
+    public class callNode extends ASTNode{
+        
+    }
+    
+    public class variableNode extends ASTNode{
+        
+    }
+    
+    public class literalNode extends ASTNode{
+        
+    }
+    
+    public class unopNode extends ASTNode{
+        
+    }
+    
+    public class binopNode extends ASTNode{
+        
+    }
+    
+    
+}

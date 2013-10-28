94317451ac5bc30b2b59e3a73c2f9f07bf56b23d
Emery Berg <emery.berg@gmail.com>
2013-10-28 00:59:13 -0700

    Template code filled in, perhaps though ASTNode.accept better option :/

--- BergVerhelstCompiler/src/Compiler/SemAnalyzer.java
+++ BergVerhelstCompiler/src/Compiler/SemAnalyzer.java
@@ -13,6 +13,7 @@ public class SemAnalyzer {
     /**
      * Used to analysis a AST against the semantic rules of the language
      * @param root as ProgramNode to use as the root of the program 
+     * @created by Emery
      */
     public SemAnalyzer(ASTNode.ProgramNode root) {
         scope = new Stack();
@@ -42,6 +43,145 @@ public class SemAnalyzer {
             scope.peek().add(var.ID);
             var = var.nextVarDec;
         }
+        
+    }
+    
+    /**
+    * Class to view ProgramNode
+    * @Class Emery
+    */
+    public void ProgramNode(ASTNode.ProgramNode program) {
+        
+    }
+    
+    /**
+     * Class to view FuncDeclarationNode
+     * @Class Emery
+     */
+    public void FuncDeclarationNode(ASTNode.FuncDeclarationNode func) {
+        
+    }
+    
+    /**
+     * Class to view VarDeclarationNode
+     * @Class Emery
+     */
+    public void VarDeclarationNode(ASTNode.VarDeclarationNode var) {
+        
+    }
+    
+    /**
+     * Class to view ParameterNode
+     * @Class Emery
+     */
+    public void ParameterNode(ASTNode.ParameterNode param) {
+        
+    }
+    
+    /**
+     * Class to view CompoundNode
+     * Statements has to happen at least once
+     * @Class Emery
+     */
+    public void CompoundNode(ASTNode.CompoundNode compound) {
+        
+    }
+    
+    /**
+     * Class to view AssignmentNode
+     * @Class Emery
+     */
+    public void AssignmentNode(ASTNode.AssignmentNode assignment) {
+        
+    }
+    
+    /**
+     * Class to view ifNode
+     * @Class Leon
+     */
+    public void IfNode(ASTNode.IfNode ifNode) {
+       
+    }
+    
+    /**
+     * Class to view loop syntax
+     * @Created Leon
+     */
+    public void LoopNode(ASTNode.LoopNode loop) {
+        
+    }
+    
+     /**
+      * The Marker Node class
+      * Markers can be the following specifiers: CONTINUE | EXIT | ENDFILE
+      * @Created Leon
+     */
+    public void MarkerNode(ASTNode.MarkerNode marker) {
+        
+    }
+    
+    /**
+     * A Return Node has an optional expression
+     * @Created Leon
+     */
+    public void ReturnNode(ASTNode.ReturnNode returnNode) {
+       
+    }
+    
+    /**
+     * Branch node for branch nodes
+     * @Created Emery
+     */
+    public void BranchNode(ASTNode.BranchNode branch) {
+        
+    }
+    
+    /**
+     * Case Node for case statements
+     * @Created Leon
+     */
+    public void CaseNode(ASTNode.CaseNode caseNode) {
+        
+    }
+    
+    /**
+     * Call Node
+     * EX. INT FOO(BAR foobar);
+     * @Created Leon
+     */
+    public void CallNode(ASTNode.CallNode call) {
+        
+    }
+    
+    /**
+     * This stores a variable ex: INT x;
+     * @Created Leon
+     */
+    public void VariableNode(ASTNode.VariableNode var) {
+        
+    }
+    
+    /**
+     * Literals can be NUM, BLIT
+     * @Created Leon
+     */
+    public void LiteralNode(ASTNode.LiteralNode lit) {
+    
+    }
+    
+    /**
+     * Unary Operation Node
+     * @Created Leon
+     */
+    public void UnopNode(ASTNode.UnopNode unop) {
+        
+    }
+    
+    /**
+     * Binary Operation Node
+     * @Created Leon
+     */
+    public void BinopNode(ASTNode.BinopNode binop) {
         
     }
 }
